57346719448614961997b33215e2bd1f51f217f5
Carsten Kiefer <carsten.kiefer@cake.systems>
2022-01-25 14:31:03 +0300

    OrderLine type

---
+++ Resource/OrderLine.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright (c) 2020 cake.systems
+ */
+
+namespace Hc\DrApiClient\Resource;
+
+/**
+ * Class OrderLine
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $sku
+ * @property string $quantity
+ * @property string $price
+ * @property string $tax
+ * @property string $transaction_id
+ * @property string $external_id
+ * @package Hc\DrApiClient\Resource
+ */
+class OrderLine extends Resource {
+
+}
+
